a42e63df251d07c1514568f733fadb4265e9d81d
Luqi Pan <luqi.pan@gmail.com>
2014-01-25 15:11:06 -0500

    Another try on cross arrow

--- server/server.rb
+++ server/server.rb
@@ -3,12 +3,17 @@ require 'sinatra'
 configure do
   set :hits, Hash.new
 
+  deaths = Hash.new {|h,k| h[k]=0}
+  set :deaths, deaths
+
   # hp = Hash.new {|h,k| h[k] = 100}
   # set :HP, hp
 end
 
 get '/hit/:id/:damage' do |id, damage|
   settings.hits[id] = Time.now.to_i.to_s
+  settings.deaths[id] += 1
+  'OK'
   # settings.HP[id] = settings.HP[id] - damage.to_i
   # if settings.HP[id] <= 0
   #   'Target dead'
@@ -18,7 +23,7 @@ get '/hit/:id/:damage' do |id, damage|
 end
 
 get '/check/:id' do |id|
-  settings.hits[id] or 'Never Dead'
+  settings.deaths[id].to_s
   # if settings.HP[id] <= 0
   #   settings.HP[id] = 100
   #   'Dead'
