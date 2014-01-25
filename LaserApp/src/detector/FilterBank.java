be023ad629d11e19a1e4710e91e00325b6fbb97c
Robin Martens <robin_martens@brown.edu>
2014-01-25 11:06:02 -0500

    support 4 distinct base patterns

--- LaserApp/src/detector/FilterBank.java
+++ LaserApp/src/detector/FilterBank.java
@@ -11,16 +11,16 @@ public class FilterBank {
 //	private static final double[] FILTER2 = {1,0,1};
 //	private static final double[] FILTER3 = {1,0,1,0,1};
 	
-	private static final double[] BASE_PATTERN = {0,1,0,1};
+	private static final double[] basePattern = {0,1,0,1};
 	private static final int STEP = 5;
 	private static final int N_SCALES = 10;
 	
 	private List<double[]> _filters;
 	
-	public FilterBank() {
+	public FilterBank(double[] basePattern) {
 		_filters = new ArrayList<double[]>();		
 		// procedurally generate filters at different scales
-		int baselen = BASE_PATTERN.length;
+		int baselen = basePattern.length;
 		for(int s=1; s<=N_SCALES; s++){		
 			int offset = 0;
 			int linewidth = s*STEP;	
@@ -29,8 +29,8 @@ public class FilterBank {
 			for(int i=0; i<baselen; i++){			
 				for(int j=0; j<linewidth; j++){
 					offset = i*linewidth+j;
-					scale[offset] = BASE_PATTERN[i];
-					scale[(scaleSize-offset) - 1] = BASE_PATTERN[i];
+					scale[offset] = basePattern[i];
+					scale[(scaleSize-offset) - 1] = basePattern[i];
 				}
 			}
 			_filters.add(scale);		
--- LaserApp/src/detector/FilterBankFactory.java
+++ LaserApp/src/detector/FilterBankFactory.java
@@ -2,10 +2,12 @@ package detector;
 
 public class FilterBankFactory {
 
+	static double[][] BASE_PATTERNS = { {0,1,0, 0,1}, {0,1,0, 1,0}, {0,1,0, 1,1}, {0,1,0, 0,0} };
+		
 	// return filter bank for player n
 	static FilterBank getNumber(int n){
 		// simplification
-		return new FilterBank();
+		return new FilterBank(BASE_PATTERNS[n]);
 	}
 	
 }
