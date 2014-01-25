5d5fe3c0842689cce53a94b0e6c70a029140a08d
Robin Martens <robin_martens@brown.edu>
2014-01-25 11:34:29 -0500

    draft normXCorrRow

--- LaserApp/src/detector/Matcher.java
+++ LaserApp/src/detector/Matcher.java
@@ -17,34 +17,10 @@ class Matcher {
 	static double[][] rowConv(double[][] signal, int width, int height,
 					double[] kernel, int kernelWidth, int kernelHeight){
 		
-//		System.out.println(">>> Matcher.rowConv");
-		
 		int smallWidth = width - kernelWidth + 1;
 		int smallHeight = height - kernelHeight + 1;
 		double[][] output = new double[smallHeight][smallWidth];
 		double acc = 0.0; // accumulator
-		
-		// what do signal and kernel look like?
-//		System.out.println("Signal: ");
-//		for(int j=0; j<height; j++){
-//			for(int i=0; i<width; i++){
-//				System.out.printf("%f ", signal[j][i]);	
-//			}
-//			System.out.println();
-//		}
-		
-		
-//		System.out.println("kernel: ");
-//		int len = kernel.length;
-//		for(int i=0; i<len; i++){
-//			System.out.printf("%f ",  kernel[i]);
-//		}
-//		System.out.println();
-		
-//		System.out.printf("smallHeight: %d\n", smallHeight);
-//		System.out.printf("smallWidth: %d\n", smallWidth);
-
-		
 		// perform row-wise matching (nothing in the y dimension)
 		for(int r=0; r<smallHeight; r++){			
 			// match every position in the line
@@ -59,14 +35,10 @@ class Matcher {
 				output[r][c] = acc;
 			}				
 		}
-		
-//		System.out.println("<<< Matcher.rowConv");
 		return output;
 	}
-		
-	// TODO: Implement 1D normalized cross-correlation
-	
 
+	
 	static double[][] ssdCorrRow(double[][]signal, int width, int height,
 									double[] kernel, int kernelWidth, int kernelHeight){
 		
@@ -78,8 +50,6 @@ class Matcher {
 		double meanSignal = 42.0;
 		double meanKernel = 13.0;
 		double inc = 0.0;
-		
-		
 		// perform row-wise matching (nothing in the y dimension)
 		for(int r=0; r<smallHeight; r++){			
 			// match every position in the line
@@ -92,15 +62,75 @@ class Matcher {
 					inc *= inc;
 					acc += inc;
 				}
-				
-				// account for normalization
-				
 				// store similarity measure in output
 				output[r][c] = acc;
 			}				
 		}
-		
+	
 		return output;
 	}
+	
+	
+	static double[][] normXCorrRow(double[][]signal, int width, int height,
+									double[] kernel, int kernelWidth, int kernelHeight){
+		
+		int smallWidth = width - kernelWidth + 1;
+		int smallHeight = height - kernelHeight + 1;
+		double[][] output = new double[smallHeight][smallWidth];
+		
+		double meanPatch = 0.0;
+		double meanKernel = 0.0;
+		
+		double acc = 0.0; // accumulator
+		double inc = 0.0;
+		
+		double acc1 = 0.0;
+		double inc1 = 0.0;
+		
+		double acc2 = 0.0;
+		double inc2 = 0.0;	
+		
+		double sumPatch = 0.0;
+		double sumKernel = 0.0;
+		
+		for(int k=0; k<kernelWidth; k++){
+			sumKernel += kernel[k];
+		}
+		meanKernel = sumKernel/kernelWidth;
+		
+		for(int r=0; r<smallHeight; r++){			
+			// match every position in the line
+			for(int c=0; c<smallWidth; c++){	
+				
+				// compute mean of patch
+				sumPatch = 0.0;
+				for(int k=0; k<kernelWidth; k++){
+					sumPatch += signal[r][c+k];
+				}
+				meanPatch = sumPatch/kernelWidth;
+				
+				acc = 0.0;
+				for(int k=0; k<kernelWidth; k++){
+					
+					// numerator
+					inc = ((signal[r][c+k]-meanPatch) * (kernel[k]-meanKernel));
+					acc += inc*inc;
+					
+					// denominator left
+					inc1 = (signal[r][c+k]-meanPatch);
+					acc1 = inc1*inc1;
+					
+					// denominator right
+					inc2 = (kernel[c+k]-meanKernel);
+					acc2 = inc2*inc2;
+				}
+
+				// store similarity measure in output
+				output[r][c] = acc / Math.sqrt(acc1 + acc2);
+			}				
+		}
+	
+		return output;
+	}	
 	
 }
