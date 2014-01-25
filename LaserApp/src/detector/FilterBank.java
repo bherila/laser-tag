3580abe239f4af1d2e89a42ed85e609d4e894027
Robin Martens <robin_martens@brown.edu>
2014-01-25 11:19:18 -0500

    add ssdCorrRow

--- LaserApp/src/detector/FilterBank.java
+++ LaserApp/src/detector/FilterBank.java
@@ -6,12 +6,6 @@ import java.util.*;
 // It returns a pyramid of responses (represented as a list)
 public class FilterBank {
 
-	// a few trivial filters for testing purposes only
-//	private static final double[] FILTER1 = {1};
-//	private static final double[] FILTER2 = {1,0,1};
-//	private static final double[] FILTER3 = {1,0,1,0,1};
-	
-	private static final double[] basePattern = {0,1,0,1};
 	private static final int STEP = 5;
 	private static final int N_SCALES = 10;
 	
--- LaserApp/src/detector/Matcher.java
+++ LaserApp/src/detector/Matcher.java
@@ -58,4 +58,41 @@ class Matcher {
 		
 	// TODO: Implement 1D normalized cross-correlation
 	
+
+	static double[][] ssdCorrRow(double[][]signal, int width, int height,
+									double[] kernel, int kernelWidth, int kernelHeight){
+		
+		int smallWidth = width - kernelWidth + 1;
+		int smallHeight = height - kernelHeight + 1;
+		double[][] output = new double[smallHeight][smallWidth];
+		double acc = 0.0; // accumulator
+		
+		double meanSignal = 42.0;
+		double meanKernel = 13.0;
+		double inc = 0.0;
+		
+		
+		// perform row-wise matching (nothing in the y dimension)
+		for(int r=0; r<smallHeight; r++){			
+			// match every position in the line
+			for(int c=0; c<smallWidth; c++){	
+				
+				// compute match
+				acc = 0.0;
+				for(int k=0; k<kernelWidth; k++){
+					inc = ((signal[r][c+k]-meanSignal) * (kernel[k]-meanKernel));
+					inc *= inc;
+					acc += inc;
+				}
+				
+				// account for normalization
+				
+				// store similarity measure in output
+				output[r][c] = acc;
+			}				
+		}
+		
+		return output;
+	}
+	
 }
