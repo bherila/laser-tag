89ffd10e44e22b29fefb0c3a7b2fcc6481bc9ad4
Robin Martens <robin_martens@brown.edu>
2014-01-25 10:06:38 -0500

    can test bank detector; TODO: Scale up filter

--- LaserApp/src/detector/BankDetector.java
+++ LaserApp/src/detector/BankDetector.java
@@ -10,6 +10,7 @@ public class BankDetector implements IDetector {
 	
 	BankDetector(int nPlayers){
 		_nPlayers = nPlayers;
+		_filterBanks = new ArrayList<FilterBank>();
 		for(int i=0; i<nPlayers; i++){
 			_filterBanks.add(FilterBankFactory.getNumber(i));
 		}
@@ -17,6 +18,7 @@ public class BankDetector implements IDetector {
 	
 	@Override
 	public int detect(int[] red, int[] green, int[] blue) {
+		System.out.println(">>> BankDetector.detect");
 		
 		// TODO: Figure out how to deal with colors
 		double[] dred = Util.normalizePixelArray(red);
@@ -27,14 +29,31 @@ public class BankDetector implements IDetector {
 		// use a different template for each
 		double[] maxima = new double[_nPlayers];
 		// use a different filter bank for each player
+		
 		for(int i=0; i<_nPlayers; i++){		
+			
+			System.out.println("BankDetector: match signal for player " + i);		
 			ResponsePyramid resp = _filterBanks.get(i).match(redSignal);
+			System.out.println("BankDetector: signal for player " + i + " has been matched");
+			
+			
+			System.out.println("BankDetector.detect: search for global max");
 			maxima[i] = resp.getGlobalMax();
+			System.out.println("BankDetector.detect: global max found");
+
+		}
+		
+		for(int i=0; i<_nPlayers; i++){
+			System.out.println("BankDetector.detect: Max response for player " + i + ": " + maxima[i]);
 		}
 		
 		// find which player is hit
 		int whoWasHit = Util.indexOfMax(maxima);
 		System.out.printf("Player %d has been hit\n", whoWasHit);
+		
+		System.out.println("BankDetector.detect: max found; stop and think!");
+		System.exit(0);
+		
 		return whoWasHit;	
 	}
 
