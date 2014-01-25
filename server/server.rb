bfeaee58cceb717020ac99fc28a3054aae6b4f71
Eli Fox-Epstein <eli@fox-epste.in>
2014-01-25 14:37:54 -0500

    Strip out damages

--- server/server.rb
+++ server/server.rb
@@ -3,26 +3,26 @@ require 'sinatra'
 configure do
   set :hits, Hash.new
 
-  hp = Hash.new {|h,k| h[k] = 100}
-  set :HP, hp
+  # hp = Hash.new {|h,k| h[k] = 100}
+  # set :HP, hp
 end
 
 get '/hit/:id/:damage' do |id, damage|
-  # settings.hits[id] = Time.now.to_i.to_s
-  settings.HP[id] = settings.HP[id] - damage.to_i
-  if settings.HP[id] <= 0
-    'Target dead'
-  else
-    'Target alive'
-  end
+  settings.hits[id] = Time.now.to_i.to_s
+  # settings.HP[id] = settings.HP[id] - damage.to_i
+  # if settings.HP[id] <= 0
+  #   'Target dead'
+  # else
+  #   'Target alive'
+  # end
 end
 
 get '/check/:id' do |id|
-  # settings.hits[id] or 'Fully Alive'
-  if settings.HP[id] <= 0
-    settings.HP[id] = 100
-    'Dead'
-  else
-    'Alive'
-  end
+  settings.hits[id] or 'Never Dead'
+  # if settings.HP[id] <= 0
+  #   settings.HP[id] = 100
+  #   'Dead'
+  # else
+  #   'Alive'
+  # end
 end
