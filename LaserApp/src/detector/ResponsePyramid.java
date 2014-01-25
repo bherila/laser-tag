20ff78ccd2b987a772c9d4a1d4f8a580790f5e51
Robin Martens <robin_martens@brown.edu>
2014-01-25 09:34:28 -0500

    can locate global max

--- LaserApp/src/detector/ResponsePyramid.java
+++ LaserApp/src/detector/ResponsePyramid.java
@@ -30,4 +30,35 @@ public class ResponsePyramid {
 		return max;
 	}
 	
+	// Find coordinates of global max
+	// {scale, row, colum}
+	double locateGlobalMax(){
+		double max = Double.NEGATIVE_INFINITY;
+		
+		int whichScale = 0;
+		int whichRow = 0;
+		int whichColumn = 0;
+		
+		double[][] scale;		
+		int nScales = _resp.size();
+		for(int s=0; s<nScales; s++){
+			scale = _resp.get(s);
+			int height = scale.length;
+			int width = scale[0].length;			
+			for(int r=0; r<height; r++){
+				for(int c=0; c<width; c++){
+					max = (scale[r][c] > max) ? scale[r][c] : max;				
+					if(scale[r][c] > max){
+						max = scale[r][c];
+						whichScale = s;
+						whichRow = r;
+						whichColumn = c;	
+					}
+				}
+			}	
+		}	
+		double[] resp = {whichScale, whichRow, whichColumn};
+		return resp;
+	}
+	
 }
